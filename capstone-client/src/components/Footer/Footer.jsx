18bc09053be22a13200311aa40b851aa6b2ff478
Grace-3908254 <choore.11@gmail.com>
2024-10-27 13:00:52 -0400

    style footer component

--- capstone-client/src/components/Footer/Footer.jsx
+++ capstone-client/src/components/Footer/Footer.jsx
@@ -3,19 +3,23 @@ import "./Footer.scss";
 
 export default function Footer() {
   return (
-    <div>
-      <section>
-        <h2>Contact</h2>
-        <div>
-          <p>Email: </p>
-          <a href="mailto:choore.11@gmail.com?subject=Emailing you about...&body=Hello Grace,">
+    <div className="footer">
+      <h2 className="footer__contact-title">Contact</h2>
+      <section className="footer__contact">
+        <div className="footer__contact-email">
+          <p className="footer__contact-email-name">Email: </p>
+          <a
+            className="footer__contact-email-link"
+            href="mailto:choore.11@gmail.com?subject=Emailing you about...&body=Hello Grace,"
+          >
             choore.11@gmail.com
           </a>
         </div>
 
-        <div>
-          <p>LinkedIn Profile: </p>
+        <div className="footer__contact-linkedin">
+          <p className="footer__contact-linkedin-name">LinkedIn: </p>
           <a
+            className="footer__contact-linkedin-link"
             href="https://www.linkedin.com/in/grace-li-ba26a1323/"
             target="_blank"
             rel="noopener noreferrer"
